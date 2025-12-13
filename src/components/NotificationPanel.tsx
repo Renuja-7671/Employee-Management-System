aaf6f2c3dfdde28bf1d6c05336a9744d797dfb29
Renuja-7671 <renujajanith7671@gmail.com>
2025-12-13 10:43:48 +0530

    notification list made mobile responsive v4

--- src/components/NotificationPanel.tsx
+++ src/components/NotificationPanel.tsx
@@ -258,7 +258,7 @@ export function NotificationPanel({
   );
 
   return (
-    <Card className="absolute right-0 left-0 sm:left-auto top-12 w-full sm:w-[450px] shadow-lg z-50 max-h-[80vh] sm:max-h-[600px] flex flex-col mx-2 sm:mx-0">
+    <Card className="absolute right-2 sm:right-0 top-12 w-[calc(100vw-16px)] sm:w-[450px] shadow-lg z-50 max-h-[80vh] sm:max-h-[600px] flex flex-col">
       <CardHeader className="p-3 sm:p-4 border-b">
         <div className="flex justify-between items-center">
           <CardTitle className="text-sm sm:text-base">Notifications</CardTitle>
